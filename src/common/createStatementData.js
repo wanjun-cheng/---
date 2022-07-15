b5bf0f465104e4880fefc356f73c5ba42394010d
Cheng <wanjun.cheng.ext@nokia-sbell.com>
2022-07-15 14:38:37 +0800

    feat: 分解 statement 函数

--- src/common/createStatementData.js
+++ src/common/createStatementData.js
@@ -1,51 +1,3 @@
-const p1 = new Promise((resolve, reject) => {
-  $.getJSON('./src/config/plays.json', function (data) {
-    resolve(data);
-  })
-})
-
-const p2 = new Promise((resolve, reject) => {
-  $.getJSON('./src/config/invoices.json', function (data) {
-    resolve(data);
-  })
-})
-
-Promise.all([p1, p2]).then((result) => {
-  console.log('start');
-  console.log(result);
-
-  if (result && result.length) {
-    const plays = result[0];
-    const invoice = result[1][0];
-    // 打印账单详情
-    const text = htmlStatement(invoice, plays);
-    $("#page").html(text);
-  }
-});
-
-
-function htmlStatement(invoice, plays) {
-  return renderHtml(createStatementData(invoice, plays));
-}
-
-function renderHtml(data) {
-  let result = `<h2>Statement for ${data.customer}</h2>`;
-  for (let perf of data.performances) {
-    result += `<p>${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)</p>`;
-  }
-
-  result += `<p>Amount owes is ${usd(data.totalAmount / 100)}</p>`;
-  result += `<p>You earned ${data.totalVolumeCredits} credits</p>`;
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
-}
-
-
-
-// 需提到独立文件
 function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
@@ -113,4 +65,8 @@ function createStatementData(invoice, plays) {
     return totalAmount;
   }
 
+}
+
+module.exports = {
+  createStatementData,
 }
